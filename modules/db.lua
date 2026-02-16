f85d9cd8094a12a118de85a7e3e3fcf6eeb52899
Alex Wilde <zackvery@gmail.com>
2026-02-17 00:01:25 +0300

    fs

--- modules/db.lua
+++ modules/db.lua
@@ -1,37 +1,37 @@
 local fs = require('fs')
+local path = require('path')
 local json = require('json')
 
 local db = {}
 
+local db_path = './data/db.json'
+
+-- Ensure
+local function ensure_data_folder()
+    local folder = path.dirname(db_path)
+    if not fs.existsSync(folder) then
+        fs.mkdirSync(folder)
+    end
+end
+
 function db:load()
     self.data = {}
-    local success, data = pcall(function()
-        local f = io.open('./data/db.json', 'r')
-        if f then
-            local c = f:read("*a")
-            f:close()
-            return c
+
+    if fs.existsSync(db_path) then
+        local success, content = pcall(fs.readFileSync, db_path, 'utf8')
+        if success and content then
+            self.data = json.decode(content) or {}
         end
-    end)
-
-    if data then
-        self.data = json.decode(data or '{}') or {}
-    end
-
-    if not self.data then
-        self.data = {}
     end
 end
 
 function db:save()
-    if self.data then
-        local s, r = pcall(function()
-            local f = io.open('./data/db.json', 'w')
-            f:write(json.encode(self.data))
-            f:flush()
-        end)
-    end
-end
+    if not self.data then return end
+    ensure_data_folder()
 
+    pcall(function()
+        fs.writeFileSync(db_path, json.encode(self.data))
+    end)
+end
 
 return db
